50c28576d07dc8054089e81b50e277c33360df23
Gizem <whiteswan1697@gmail.com>
2021-10-29 17:25:46 +0300

    Component styles changed. Project done

--- src/App.js
+++ src/App.js
@@ -10,24 +10,54 @@ const SEARCH_API ="https://api.themoviedb.org/3/search/movie?api_key=cb6977a5b0b
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    fetch(FEATURED_API)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setMovies(data.results);
-    });
-
+    getMovies(FEATURED_API);
   }, []);
 
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+    });
+  }
+
+  const handleOnSubmit = (e) =>{
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies(SEARCH_API+searchTerm)
+      setSearchTerm('');
+    }
+
+    
+  }
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return ( 
-    <div className="movie-container">
+    <>
+      <div className="search-container">
+        <form onSubmit={handleOnSubmit}>
+          <input 
+            className="search" 
+            type="search" 
+            placeholder="Search" 
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </div>
+      <div className="movie-container">
       {movies.length > 0 && movies.map((movie) =>
         <Movie key={movie.id} {...movie} />
       )}
-    </div>
+      </div>
+    </>
   );
 }
 
